0dbc85bfbfa6110037ddad042cbe3906c4952ad1
germaneiter <german.eiter@finceptiv.com>
2023-01-18 20:50:05 +0200

    created user signup route and controller, added variables from .env file #17

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -4,6 +4,11 @@ import User from '../models/userModel.js'
 import ProfileModel from '../models/ProfileModel.js';
 
 dotenv.config()
+const SECRET = process.env.SECRET;
+const HOST =  process.env.SMTP_HOST
+const PORT =  process.env.SMTP_PORT
+const USER =  process.env.SMTP_USER
+const PASS =  process.env.SMTP_PASS
 
 export const signin = async (req, res) => {
     const {email, password} = req.body
@@ -18,6 +23,22 @@ export const signin = async (req, res) => {
         res.status(200).json({result: existingUser, userProfile, token})
     } catch (error) {
         res.status(500).json({message: error})
+    }
+}
+
+export const signup = async (req, res) => {
+    const {email, password, confirmPassword, firstName, lastName, bio} = req.body
+    try {
+        const existingUser = await User.findOne({email})
+        const userProfile = await ProfileModel.findOne({userId: existingUser?._id})
+        if (existingUser) return res.status(400).json({message: "User already exist hui"})
+        if (password !== confirmPassword) return res.status(400).json({message: "Password don't match"})
+        const hashedPassword = await bcrypt.hash(password, 12)
+        const result = await User.create({email, password: hashedPassword, name: `${firstName} ${lastName}`, bio})
+        const token = jwt.sign({email: result.email, id: result._id}, SECRET, {expiresIn: "1h"})
+        res.status(200).json({result, userProfile, token})
+    } catch (error) {
+        res.status(500).json({message: "Something went wrong"})
     }
 }
 
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { signin } from '../controllers/user.js'
+import {signin, signup} from '../controllers/user.js'
 
 const router = express.Router()
 
 router.post('/signin', signin)
+router.post('/signup', signup)
 
 
 export default router
