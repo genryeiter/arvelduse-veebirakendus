7e3dca1a65022b2025bedb656dd6f54d3780446a
germaneiter <german.eiter@finceptiv.com>
2023-01-18 21:03:36 +0200

    created profile 'update', 'delete', 'getBySearch' routes and controllers, #16

--- backend/controllers/profile.js
+++ backend/controllers/profile.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import ProfileModel from '../models/ProfileModel.js';
 
 export const getProfile = async (req, res) => {
@@ -18,4 +19,31 @@ export const getProfilesByUser = async (req, res) => {
     } catch (error) {
         res.status(404).json({message: error.message});
     }
+}
+
+export const getProfilesBySearch = async (req, res) => {
+    const {searchQuery} = req.query;
+    try {
+        const name = new RegExp(searchQuery, "i");
+        const email = new RegExp(searchQuery, "i");
+        const profiles = await ProfileModel.find({$or: [{name}, {email}]});
+        res.json({data: profiles});
+    } catch (error) {
+        res.status(404).json({message: error.message});
+    }
+}
+
+export const updateProfile = async (req, res) => {
+    const {id: _id} = req.params
+    const profile = req.body
+    if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No client with that id')
+    const updatedProfile = await ProfileModel.findByIdAndUpdate(_id, {...profile, _id}, {new: true})
+    res.json(updatedProfile)
+}
+
+export const deleteProfile = async (req, res) => {
+    const {id} = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No profile with id: ${id}`);
+    await ProfileModel.findByIdAndRemove(id);
+    res.json({message: "Profile deleted successfully."});
 }
--- backend/routes/profile.js
+++ backend/routes/profile.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import {getProfile, getProfilesByUser} from '../controllers/profile.js'
+import {updateProfile, deleteProfile, getProfile, getProfilesByUser} from '../controllers/profile.js'
 
 const router = express.Router()
 
 router.get('/:id', getProfile)
 router.get('/', getProfilesByUser)
+router.patch('/:id', updateProfile)
+router.delete('/:id', deleteProfile)
 
 
 export default router
