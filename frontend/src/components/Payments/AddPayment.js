3492c8101b6c7eaca92095f541a43a6c4811a95f
germaneiter <german.eiter@finceptiv.com>
2023-01-19 18:09:37 +0200

    added payment pages, #58

---
+++ frontend/src/components/Payments/AddPayment.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react'
+import Modal from './Modal'
+
+const AddPayment = () => {
+
+    const [open, setOpen] = useState()
+
+    return (<div>
+            <Modal open={open} setOpen={setOpen}/>
+        </div>)
+}
+
+export default AddPayment
