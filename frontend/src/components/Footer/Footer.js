6e1f82e8fcf7f4a57c68692efbd5a9e86b102944
germaneiter <german.eiter@finceptiv.com>
2023-01-19 17:30:03 +0200

    added footer, #11

---
+++ frontend/src/components/Footer/Footer.js
@@ -0,0 +1,23 @@
+import React, {useState, useEffect} from 'react'
+import {useLocation} from 'react-router-dom'
+import styles from './Footer.module.css'
+import FabButton from '../Fab/Fab'
+
+const Footer = () => {
+    const location = useLocation()
+    const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')))
+
+    useEffect(() => {
+        setUser(JSON.parse(localStorage.getItem('profile')))
+    }, [location])
+
+    return (
+        <footer>
+            {user && (
+                <FabButton/>
+            )}
+        </footer>
+    )
+}
+
+export default Footer
