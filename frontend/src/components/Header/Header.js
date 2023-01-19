d6452f42d1c82baea3f96a47608d3e5bb1bd9dc2
germaneiter <german.eiter@finceptiv.com>
2023-01-19 21:36:15 +0200

    updated Header components in frontend

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,30 +1,136 @@
-import React from 'react'
-import {useHistory} from 'react-router-dom'
+import React, {useState, useEffect} from 'react'
+import {useHistory, useLocation} from 'react-router-dom'
+import {useDispatch} from 'react-redux'
+import decode from 'jwt-decode'
 import styles from './Header.module.css'
 import logo from '../../icons/pngIcons/logo500x.png'
+import Button from '@material-ui/core/Button';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import Grow from '@material-ui/core/Grow';
+import Paper from '@material-ui/core/Paper';
+import Popper from '@material-ui/core/Popper';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuList from '@material-ui/core/MenuList';
+import {makeStyles} from '@material-ui/core/styles';
+import Avatar from '@material-ui/core/Avatar';
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+    },
+    paper: {
+        marginRight: theme.spacing(2),
+    },
+}));
+
 
 const Header = () => {
+    const dispatch = useDispatch()
+    const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')))
     const history = useHistory()
+    const location = useLocation()
+
+
+    useEffect(() => {
+        setUser(JSON.parse(localStorage.getItem('profile')))
+    }, [location])
+
+    const logout = () => {
+        dispatch({type: 'LOGOUT'})
+        history.push('/')
+        setUser(null)
+    }
+
+    useEffect(() => {
+        const token = user?.token
+        if (token) {
+            const decodedToken = decode(token)
+            if (decodedToken.exp * 1000 < new Date().getTime()) logout()
+        }
+        // eslint-disable-next-line
+    }, [location, user])
+
+    const classes = useStyles();
     const [open, setOpen] = React.useState(false);
     const anchorRef = React.useRef(null);
+
+    const handleToggle = () => {
+        setOpen((prevOpen) => !prevOpen);
+    };
+
+    const handleClose = (event) => {
+        if (anchorRef.current && anchorRef.current.contains(event.target)) {
+            return;
+        }
+
+        setOpen(false);
+    };
+
+
+    const openLink = (link) => {
+        history.push(`/${link}`)
+        setOpen(false);
+    }
+
+    function handleListKeyDown(event) {
+        if (event.key === 'Tab') {
+            event.preventDefault();
+            setOpen(false);
+        }
+    }
 
     const prevOpen = React.useRef(open);
     React.useEffect(() => {
         if (prevOpen.current === true && open === false) {
             anchorRef.current.focus();
         }
-
         prevOpen.current = open;
     }, [open]);
 
-    return (
+
+    if (!user) return (
         <div className={styles.header2}>
             <img style={{width: '50px', cursor: 'pointer'}} onClick={() => history.push('/')}
                  src={logo} alt="arc-invoice"/>
             <button onClick={() => history.push('/login')} className={styles.login}>Login</button>
         </div>
     )
-
+    return (
+        <div className={styles.header}>
+            <div className={classes.root}>
+                <div>
+                    <Button
+                        ref={anchorRef}
+                        aria-controls={open ? 'menu-list-grow' : undefined}
+                        aria-haspopup="true"
+                        onClick={handleToggle}
+                    >
+                        <Avatar style={{backgroundColor: '#1976D2'}}>{user?.result?.name?.charAt(0)}</Avatar>
+                    </Button>
+                    <Popper open={open} anchorEl={anchorRef.current} role={undefined} transition disablePortal>
+                        {({TransitionProps, placement}) => (
+                            <Grow
+                                {...TransitionProps}
+                                style={{transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom'}}
+                            >
+                                <Paper elevation={2}>
+                                    <ClickAwayListener onClickAway={handleClose}>
+                                        <MenuList autoFocusItem={open} id="menu-list-grow"
+                                                  onKeyDown={handleListKeyDown}>
+                                            <MenuItem
+                                                onClick={() => openLink('settings')}>Profile Settings</MenuItem>
+                                            <MenuItem onClick={() => logout()}>Logout</MenuItem>
+                                        </MenuList>
+                                    </ClickAwayListener>
+                                </Paper>
+                            </Grow>
+                        )}
+                    </Popper>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Header
