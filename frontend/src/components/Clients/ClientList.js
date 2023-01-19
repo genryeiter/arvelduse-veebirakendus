5df7f55f5aa03367729ea62ed14dc022dbad8ea2
germaneiter <german.eiter@finceptiv.com>
2023-01-19 21:11:27 +0200

    updated client components in frontend

--- frontend/src/components/Clients/ClientList.js
+++ frontend/src/components/Clients/ClientList.js
@@ -1,10 +1,10 @@
- /* eslint-disable */
-import React, { useState, useEffect} from 'react'
+/* eslint-disable */
+import React, {useState, useEffect} from 'react'
 import Clients from './Clients'
 import AddClient from './AddClient'
-import { getClientsByUser } from '../../actions/clientActions'
-import { useDispatch, useSelector } from 'react-redux'
-import { useLocation, useHistory } from 'react-router-dom'
+import {getClientsByUser} from '../../actions/clientActions'
+import {useDispatch, useSelector} from 'react-redux'
+import {useLocation, useHistory} from 'react-router-dom'
 import NoData from '../svgIcons/NoData'
 import Spinner from '../Spinner/Spinner'
 
@@ -19,52 +19,52 @@ const ClientList = () => {
     const user = JSON.parse(localStorage.getItem('profile'))
     const {clients} = useSelector((state) => state.clients)
     const isLoading = useSelector(state => state.clients.isLoading)
-    // const clients = []
 
-    
-    // useEffect(() => {
-    // }, [currentId, dispatch]);
-    
-//     useEffect(() => {
-//         dispatch(getClients(1));
-//         // dispatch(getClientsByUser({userId : user?.result?._id}));
-//         // dispatch(getClientsByUser({ search :user?.result?._id, tags: tags.join(',') }));
-//     },[location]
-// )
+    useEffect(() => {
+        dispatch(getClientsByUser({search: user?.result?._id || user?.result?.sub}));
+    }, [location, dispatch])
 
-useEffect(() => {
-    dispatch(getClientsByUser({ search: user?.result?._id || user?.result?.sub }));
-  },[location, dispatch])
+    if (!user) {
+        history.push('/login')
+    }
 
-  if(!user) {
-    history.push('/login')
-  }
+    if (isLoading) {
+        return <div style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            flexDirection: 'column',
+            paddingTop: '20px'
+        }}>
+            <Spinner/>
+        </div>
+    }
 
-  
-  if(isLoading) {
-    return  <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', paddingTop: '20px'}}>
-        <Spinner />
-    </div>
-  }
-
-  if(clients.length === 0) {
-    return  <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', paddingTop: '20px', margin: '80px'}}>
-      <NoData />
-    <p style={{padding: '40px', color: 'gray', textAlign: 'center'}}>No customers yet. Click the plus icon to add customer</p>
-  
-    </div>
-  }
+    if (clients.length === 0) {
+        return <div style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            flexDirection: 'column',
+            paddingTop: '20px',
+            margin: '80px'
+        }}>
+            <NoData/>
+            <p style={{padding: '40px', color: 'gray', textAlign: 'center'}}>No customers yet. Click the plus icon to
+                add customer</p>
+        </div>
+    }
 
     return (
         <div>
-            <AddClient 
-                open={open} 
+            <AddClient
+                open={open}
                 setOpen={setOpen}
                 currentId={currentId}
                 setCurrentId={setCurrentId}
             />
-            <Clients 
-                open={open} 
+            <Clients
+                open={open}
                 setOpen={setOpen}
                 currentId={currentId}
                 setCurrentId={setCurrentId}
