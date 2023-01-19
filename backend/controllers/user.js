0acdb581fb3a98a7fca9bf83f0ba5276822d310c
germaneiter <german.eiter@finceptiv.com>
2023-01-19 19:43:13 +0200

    updated user controller and user routes in backend, #63

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,14 +1,17 @@
 import bcrypt from 'bcryptjs'
 import dotenv from 'dotenv'
 import User from '../models/userModel.js'
-import ProfileModel from '../models/ProfileModel.js';
+import ProfileModel from '../models/ProfileModel.js'
+import crypto from "crypto"
+import jwt from "jsonwebtoken"
+import nodemailer from 'nodemailer'
 
 dotenv.config()
 const SECRET = process.env.SECRET;
-const HOST =  process.env.SMTP_HOST
-const PORT =  process.env.SMTP_PORT
-const USER =  process.env.SMTP_USER
-const PASS =  process.env.SMTP_PASS
+const HOST = process.env.SMTP_HOST
+const PORT = process.env.SMTP_PORT
+const USER = process.env.SMTP_USER
+const PASS = process.env.SMTP_PASS
 
 export const signin = async (req, res) => {
     const {email, password} = req.body
@@ -20,6 +23,7 @@ export const signin = async (req, res) => {
         if (!isPasswordCorrect) {
             return res.status(400).json({message: "Invalid credentials"})
         }
+        const token = jwt.sign({email: existingUser.email, id: existingUser._id}, SECRET, {expiresIn: "1h"})
         res.status(200).json({result: existingUser, userProfile, token})
     } catch (error) {
         res.status(500).json({message: error})
@@ -42,3 +46,68 @@ export const signup = async (req, res) => {
     }
 }
 
+export const resetPassword = (req, res) => {
+    const newPassword = req.body.password
+    const sentToken = req.body.token
+    User.findOne({resetToken: sentToken, expireToken: {$gt: Date.now()}})
+        .then(user => {
+            if (!user) {
+                return res.status(422).json({error: "Try again session expired"})
+            }
+            bcrypt.hash(newPassword, 12).then(hashedpassword => {
+                user.password = hashedpassword
+                user.resetToken = undefined
+                user.expireToken = undefined
+                user.save().then((saveduser) => {
+                    res.json({message: "password updated success"})
+                })
+            })
+        }).catch(err => {
+        console.log(err)
+    })
+}
+
+export const forgotPassword = (req, res) => {
+    const {email} = req.body
+    const transporter = nodemailer.createTransport({
+        host: HOST,
+        port: PORT,
+        auth: {
+            user: USER,
+            pass: PASS
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    })
+
+    crypto.randomBytes(32, (err, buffer) => {
+        if (err) {
+            console.log(err)
+        }
+        const token = buffer.toString("hex")
+        User.findOne({email: email})
+            .then(user => {
+                if (!user) {
+                    return res.status(422).json({error: "User does not exist in our database"})
+                }
+                user.resetToken = token
+                user.expireToken = Date.now() + 3600000
+                user.save().then((result) => {
+                    transporter.sendMail({
+                        to: user.email,
+                        from: "arvelduseveebirakendus@gmail.com",
+                        subject: "Password reset request",
+                        html: `
+                    <p>You requested for password reset from Arc Invoicing application</p>
+                    <h5>Please click this <a href="https://localhost:3000/reset/${token}">link</a> to reset your password</h5>
+                    <p>Link not clickable?, copy and paste the following url in your address bar.</p>
+                    <p>https://accountill.com/reset/${token}</p>
+                    <P>If this was a mistake, just ignore this email and nothing will happen.</P>
+                    `
+                    })
+                    res.json({message: "check your email"})
+                }).catch((err) => console.log(err))
+            })
+    })
+}
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import {signin, signup} from '../controllers/user.js'
+import {forgotPassword, resetPassword, signin, signup} from '../controllers/user.js'
 
 const router = express.Router()
 
 router.post('/signin', signin)
 router.post('/signup', signup)
+router.post('/forgot', forgotPassword);
+router.post('/reset', resetPassword);
 
 
 export default router
